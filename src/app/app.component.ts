33dfd1d1c1e85961145f45b8ccd2c6822881b56d
Siarhei_Prakharenka@epam.com <Wellbe1ng>
2017-09-23 18:08:48 +0300

    app simple properties to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My First Angular App';
+  name = 'App';
+  description = 'My First Angular App';
+  price =  Infinity;
+  category = Category.HEALTH;
+  isAvailable = true;
+  ingredients = ['SUN', 'WATER', 'AIR'];
+  equivalents = ['BULB', 'BEAR', 'SMOKING'];
+
+  // constructor(public name = 'App', public description = 'My First Angular App',
+  //     public price = 1, public category = Category.HEALTH, public isAvailable = true,
+  //     public ingredients = ['SUN', 'WATER', 'AIR'], public equivalents = ['LAMPPOST', 'BEAR', '-']) {}
+
+
 }
+
+export const enum Category {'VEHICLE', 'HEALTH', 'CLOTHING', 'SHOE', 'WATCH'}
